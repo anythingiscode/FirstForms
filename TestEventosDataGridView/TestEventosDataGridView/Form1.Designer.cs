c884da53ede0df8c0e589f0c580741da81bc2f16
anythingiscode <rolkawa99@gmail.com>
2022-10-21 00:48:05 +0200

    Test Eventos Eventos CellValidating y CellValidated

---
+++ TestEventosDataGridView/TestEventosDataGridView/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace TestEventosDataGridView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.colNombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colEdad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colAñosRestantesJubilacion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colNombre,
+            this.colEdad,
+            this.colAñosRestantesJubilacion});
+            this.dataGridView1.Location = new System.Drawing.Point(48, 58);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(778, 192);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellValidated += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValidated);
+            this.dataGridView1.CellValidating += new System.Windows.Forms.DataGridViewCellValidatingEventHandler(this.dataGridView1_CellValidating);
+            // 
+            // colNombre
+            // 
+            this.colNombre.HeaderText = "Nombre";
+            this.colNombre.MinimumWidth = 6;
+            this.colNombre.Name = "colNombre";
+            this.colNombre.Width = 125;
+            // 
+            // colEdad
+            // 
+            this.colEdad.HeaderText = "Edad";
+            this.colEdad.MinimumWidth = 6;
+            this.colEdad.Name = "colEdad";
+            this.colEdad.Width = 125;
+            // 
+            // colAñosRestantesJubilacion
+            // 
+            this.colAñosRestantesJubilacion.HeaderText = "Años Restantes Jubilación";
+            this.colAñosRestantesJubilacion.MinimumWidth = 6;
+            this.colAñosRestantesJubilacion.Name = "colAñosRestantesJubilacion";
+            this.colAñosRestantesJubilacion.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colNombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colEdad;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colAñosRestantesJubilacion;
+    }
+}
+
---
+++ TestEventosDataGridView/TestEventosDataGridView/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestEventosDataGridView
+{
+    /* En un DataGridView queremos que al modificar los datos de la edad de una persona se modifique 
+     * auto los años que le quedan para su jubilación
+     */
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InsertarDatos();
+        }
+
+        private void InsertarDatos()
+        {
+            int rowIndex = dataGridView1.Rows.Add();            //Nos devuelve el indice de la fila que hemos insertado
+            dataGridView1.Rows[rowIndex].Cells[0].Value = "Silvia";     //Relleno las celdas de la fila rowIndex
+            dataGridView1.Rows[rowIndex].Cells[1].Value = 30;
+            dataGridView1.Rows[rowIndex].Cells[2].Value = 35;
+
+            rowIndex = dataGridView1.Rows.Add();            //Nos devuelve el indice de la fila que hemos insertado
+            dataGridView1.Rows[rowIndex].Cells[0].Value = "Pedro";     //Relleno las celdas de la fila rowIndex
+            dataGridView1.Rows[rowIndex].Cells[1].Value = 50;
+            dataGridView1.Rows[rowIndex].Cells[2].Value = 15;           
+        }
+
+        private void dataGridView1_CellValidating(object sender, DataGridViewCellValidatingEventArgs e)
+        {
+            /* Este evento sucede cada vez que selecciono una celda. 
+             * Para evitar esto y que solo se ejecute cuando modifiquemos la edad de una persona... controlo que el index de la columna sea == 1 que corresponde a la columna de "Edad"
+             */
+            if(e.ColumnIndex == 1)
+            {
+                //Confimo que el dato sea válido --> que sea un double y que esté entre 16 y 65 años
+                bool isDouble = double.TryParse(e.FormattedValue.ToString(), out double resultadoNumerico);
+                if (isDouble)
+                {
+                    if(resultadoNumerico < 16 || resultadoNumerico > 65)
+                    {
+                        dataGridView1.CancelEdit();     //Este metodo cancela la modificación del dato introducido, con lo que restaura el que estubiese antes
+                        e.Cancel = true;
+                        MessageBox.Show("La edad debe estar entre 16 y 65 años");
+                    }
+
+                }
+                else
+                {
+                    dataGridView1.CancelEdit();     //Este metodo cancela la modificación del dato introducido, con lo que restaura el que estubiese antes
+                    e.Cancel = true;    //Esto mantiene el foco en la celda seleccionada
+                    MessageBox.Show("El dato introducido debe ser numérico");
+                }
+            }
+            
+
+        }
+
+        /* Este vento se ejecuta cuando se valida el dato de la celda*/
+        private void dataGridView1_CellValidated(object sender, DataGridViewCellEventArgs e)
+        {
+            if(e.ColumnIndex == 1)
+            {
+                double nuevaEdad = Convert.ToDouble(dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value);
+                double añosRestantes = 65 - nuevaEdad;
+                dataGridView1.Rows[e.RowIndex].Cells[2].Value = añosRestantes;
+
+            }
+        }
+    }
+}
